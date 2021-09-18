209b0aae720afe1bbac1548e349cadb7bc1e6acc
Janis Gösser <janis@janiss-mbp.speedport.ip>
2021-09-18 22:03:56 +0200

    modify initial input data

--- pipelines/business_objects.py
+++ pipelines/business_objects.py
@@ -4,14 +4,20 @@ from dataclasses import dataclass
 
 @dataclass
 class InitialInputData:
+    source_thread_id: str
+    source_type: str
+    body: str
+    info: dict
     msg_id: str
     payload: dict
-    source: str
 
 
 @dataclass
-class GetOrCreateSlackMsg(InitialInputData):
-    pass
+class GetOrCreateSlackMsg:
+    source_thread_id: str
+    source_type: str
+    body: str
+    info: dict
 
 
 @dataclass
